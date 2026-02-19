2a35042ec3201a8fcadbb82515649991c8d71600
k25-b <k256572@nu.edu.pk>
2026-02-19 23:42:38 +0500

    Update Task3.c

--- Task3.c
+++ Task3.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
-int unit, rate;
+int hours, rate, total;
 char vehicle_type;
 int main() {
     printf("Type of the vehicle: ");
     scanf("%c", &vehicle_type);
     switch (vehicle_type) {
         case 'C':
-            printf("Regular parking: 50rs/hr\nVIP Parking: 100rs/hr");
+            printf("Regular parking: 50rs/hr\nVIP Parking: 100rs/hr\n");
+            printf("What parking do you want? ");
+            scanf("%d", &rate);
+            printf("How many hours do you want to park for? ");
+            scanf("%d", &hours);
             break;
         case 'b':
-            printf("Regular parking: 20rs/hr\nPremium: 40rs/hr");
+            printf("Regular parking: 20rs/hr\nPremium: 40rs/hr\n");
+            printf("What parking do you want? ");
+            scanf("%d", &rate);
+            printf("How many hours do you want to park for? ");
+            scanf("%d", &hours);
             break;
         case 'B':
-            printf("200rs/hr");
+            printf("200rs/hr\n");
+            printf("How many hours do you want to park for? ");
+            scanf("%d", &hours);
 
 
     }
+    total = hours * rate;
+    printf("Bill: %d", total);
 }
